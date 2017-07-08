a38e15c4b73795eb7c1ae428f5db78e297932734
adip666 <adrian.przydatek@hotmail.com>
2017-07-08 21:12:41 +0200

    log viewer + poprawki

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -16,22 +16,25 @@ public class PlayerController : MonoBehaviour
     Rigidbody _rb;
     float moveHorizontal;
     float moveVertical;
+    float StartAccelerationY;
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+#if UNITY_ANDROID
+        StartAccelerationY = Input.acceleration.y;
+#endif
     }
     void FixedUpdate()
     {
-        if(Application.platform == RuntimePlatform.WindowsEditor)
-        {
-             moveHorizontal = Input.GetAxis("Horizontal");
-             moveVertical = Input.GetAxis("Vertical");
-        }
-        else if(Application.platform == RuntimePlatform.WindowsEditor){
-            moveHorizontal = -Input.acceleration.y;
-             moveVertical = Input.acceleration.x;
-        }
-        
+#if UNITY_ANDROID
+        moveHorizontal = Input.acceleration.x; // Lewo - Prawo
+        moveVertical = Input.acceleration.y - StartAccelerationY;
+        _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt*2);
+#else
+         moveHorizontal = Input.GetAxis("Horizontal");
+         moveVertical = Input.GetAxis("Vertical");
+        _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
+#endif
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         _rb.velocity = movement * speed;
 
@@ -41,7 +44,7 @@ public class PlayerController : MonoBehaviour
             0.0f,
             Mathf.Clamp(_rb.position.z, boundary.zMin, boundary.zMax)
         );
-       // _rb.velocity.Normalize();
-        _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
+        Debug.Log("horizontal: " + moveHorizontal + "vertical" + moveVertical);
+        //_rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
     }
 }
