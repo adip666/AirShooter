43c7d98008a9530100e3b38b14c0eb25f526499e
adip666 <adrian.przydatek@hotmail.com>
2017-09-03 16:28:48 +0200

    optymalizacja + touch pad + strzelanie

---
+++ Assets/scripts/Ammo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour {
+
+    
+    public float speed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        Invoke("Hide", 5);
+    }
+    void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -12,35 +12,81 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float tilt;
     public Boundary boundary;
+    public SimpleTouchPad touchPad;
+    public TouchAreaButton areaButton;
 
+    bool _touch= false;
     Rigidbody _rb;
     float moveHorizontal;
     float moveVertical;
     float StartAccelerationY;
     Vector3 movement;
+    Quaternion calibrationQuaternion;
+
+    public GameObject shot;
+    public Transform shotSpawn, shotSpawn2;
+    public float fireRate;
+    float nextFire;
+
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
 #if UNITY_ANDROID
-        StartAccelerationY = Input.acceleration.y;
+        //StartAccelerationY = Input.acceleration.y;
+        CalibrateAccelerometer();
 #endif
     }
+    void CalibrateAccelerometer()
+    {
+        Vector3 accelerationSnapshoot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0f, 0f, -1f), accelerationSnapshoot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
+
+    Vector3 FixAcceleration(Vector3 acceleration)
+    {
+        Vector3 fixedAcceleration = calibrationQuaternion* acceleration;
+        return fixedAcceleration;
+    }
+
     void FixedUpdate()
     {
 #if UNITY_ANDROID
-        moveHorizontal = Input.acceleration.x; // Lewo - Prawo
-        moveVertical = .15f + Input.acceleration.y;
-        _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt*2);
-        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-#endif
+        //moveHorizontal = Input.acceleration.x; // Lewo - Prawo
+        //moveVertical = .15f + Input.acceleration.y;
+        //movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (!_touch)
+        {
+            Vector3 accelerationRaw = Input.acceleration;
+            Vector3 acceleration = FixAcceleration(accelerationRaw);
+            movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
+           // _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt * 2);
+            _rb.velocity = movement * speed;
 
+        }
+        else
+        {
+            Vector2 direction = touchPad.GetDirection();
+            movement = new Vector3(direction.x, 0.0f, direction.y);
+            //_rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
+            _rb.velocity = movement * speed/2;
+
+        }
+        _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt * 2);
+
+
+
+#endif
+        #region sterowanie PC
 #if UNITY_EDITOR
         moveHorizontal = Input.GetAxis("Horizontal");
          moveVertical = Input.GetAxis("Vertical");
-        _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
         movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        _rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
+        _rb.velocity = movement * speed / 2;
 #endif
-        _rb.velocity = movement * speed;
+#endregion
+        
 
         _rb.position = new Vector3
         (
@@ -50,5 +96,22 @@ public class PlayerController : MonoBehaviour
         );
       
         //_rb.rotation = Quaternion.Euler(0.0f, 0.0f, _rb.velocity.x * -tilt);
+    }
+
+    public void ChangeSteering()
+    {
+        _touch = !_touch;
+    }
+    void Update()
+    {
+        if (areaButton.CanFire() && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            //Instantiate(shot, shotSpawn2.position, shotSpawn2.rotation);
+
+
+            // audio.Play();
+        }
     }
 }
